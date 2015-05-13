b46702d1c4fc1c1ba00e9f7794d34ba11f981225
fordsfords <sford@geeky-boy.com>
2015-05-12 20:31:03 -0500

    semlit update
    Updated doc via new semlit

--- bld.sh
+++ bld.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 # bld.sh
 
-# Check to see if SEMLIT is installed.
-rm -rf html
-mkdir html
-cd html
-if semlit.sh -I .. ../catchup.sldoc; then :
-  cp *.c ..
-else :
-  echo "semlit.sh failed" >&2
-  exit 1
+mkdir html >/dev/null 2>&1
+# Check to see if semlit is installed
+if which semlit.sh >/dev/null 2>&1; then :
+  rm -rf html
+  mkdir html
+  cd html
+
+  if semlit.sh -I .. ../catchup.sldoc; then :
+    # We've generated fresh .c and .h files.  Copy them into the project dir.
+    for F in *_c.txt; do f=`basename $F _c.txt`;
+      rm -f ../$f.c; cp $F ../$f.c; chmod -w ../$f.c
+    done
+  else :
+    echo "semlit.sh failed" >&2
+    exit 1
+  fi
+  cd ..
 fi
-cd ..
 
 gcc -g -DSELFTEST -o catchup catchup.c
--- html/catchup_c.txt
+++ html/catchup_c.txt
@@ -1,4 +1,13 @@
 /* catchup.c - program to demonstrate the "catchup" rate-setting algorithm.  See ??? */
+/*
+# This code and its documentation is Copyright 2015 Steven Ford, http://geeky-boy.com
+# and licensed "public domain" style under Creative Commons "CC0": http://creativecommons.org/publicdomain/zero/1.0/
+# To the extent possible under law, the contributors to this project have
+# waived all copyright and related or neighboring rights to this work.
+# In other words, you can use this code for any purpose without any
+# restrictions.  This work is published from: United States.  The project home
+# is https://github.com/fordsfords/catchup/tree/gh-pages
+*/
 
 #include <stdlib.h>
 #include <unistd.h>
